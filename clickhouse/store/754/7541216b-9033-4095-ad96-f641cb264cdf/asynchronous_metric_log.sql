899ad1d6dd508846e9cd045470e912be99a5e709
MKvetko <m.kvetko@petrovich.tech>
2024-08-05 20:47:13 +0300

    Added MinIO + ClickHouse in one docker-compose

---
+++ clickhouse/store/754/7541216b-9033-4095-ad96-f641cb264cdf/asynchronous_metric_log.sql
@@ -0,0 +1,12 @@
+ATTACH TABLE _ UUID 'dd4ce710-b2d7-4926-ba06-b924be1f786b'
+(
+    `event_date` Date,
+    `event_time` DateTime,
+    `event_time_microseconds` DateTime64(6),
+    `metric` LowCardinality(String),
+    `value` Float64
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMM(event_date)
+ORDER BY (event_date, event_time)
+SETTINGS index_granularity = 8192
