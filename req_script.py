ee4688596b9100a310562e8e8da799384216a6c6
Aliaksei Lubianau <aliaksei.lubianau@innowise.com>
2024-08-07 14:47:21 +0200

    upd python script: saving json in MinIO

--- req_script.py
+++ req_script.py
@@ -1,19 +1,40 @@
 import requests
-import os
 import json
+import boto3
+from botocore.exceptions import NoCredentialsError
+from io import BytesIO
 
-full_url = 'https://api.nasa.gov/EPIC/api/natural/date/2019-05-30?api_key=DEMO_KEY'
-save_path = '/home/user/PycharmProjects/Project_K/API_to_CH'
+full_url = 'https://api.nasa.gov/DONKI/CMEAnalysis?startDate=2016-09-01&endDate=2016-09-30&mostAccurateOnly=true&speed=500&halfAngle=30&catalog=ALL&api_key=DEMO_KEY'
 
-# Создание папки, если не существует
-os.makedirs(save_path, exist_ok=True)
+# Параметры MinIO
+minio_endpoint = 'http://localhost:9002'
+minio_access_key = 'minioadmin'
+minio_secret_key = 'minioadmin'
+bucket_name = 'databucket'
+object_name = 'epic_data.json'
 
 response = requests.get(full_url)
 data = response.json()
 
-file_path = os.path.join(save_path, 'epic_data.json')
-with open(file_path, 'w') as file:
-    json.dump(data, file, indent=4)
+# Преобразование данных в байтовый поток
+data_str = json.dumps(data, indent=4)  # Преобразуем данные в строку JSON
+data_bytes = BytesIO(data_str.encode('utf-8'))  # Кодируем строку в байты и создаем поток
+data_bytes.seek(0)  # Вернуться в начало потока
 
-print(f"Data saved to {file_path}")
+# Конфигурация MinIO клиента
+s3_client = boto3.client(
+    's3',
+    endpoint_url=minio_endpoint,
+    aws_access_key_id=minio_access_key,
+    aws_secret_access_key=minio_secret_key
+)
+
+# Загрузка данных в MinIO
+try:
+    s3_client.upload_fileobj(data_bytes, bucket_name, object_name)
+    print(f"Data successfully uploaded to MinIO bucket '{bucket_name}' as '{object_name}'")
+except NoCredentialsError:
+    print("Credentials not available")
+except Exception as e:
+    print(f"An error occurred: {e}")
 
